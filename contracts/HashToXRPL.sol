e65e3f3d18d2258d7c195d9ff46cd4d5ada4f2ba
adshash <adil.hash@hotmail.co.uk>
2025-02-08 20:49:19 +0000

    Added HashToXRPL hopefully

--- contracts/HashToXRPL.sol
+++ contracts/HashToXRPL.sol
@@ -5,8 +5,20 @@ contract HashToXRPL {
     // Event emitted when a hash key is received
     event HashKeySubmitted(bytes32 indexed hashKey);
 
+    // Mapping to store submitted hash keys
+    mapping(bytes32 => bool) public storedHashes;
+
     // Function to accept the hash key
     function submitHashKey(bytes32 hashKey) public {
+        // Store the hash key
+        storedHashes[hashKey] = true;
+
+        // Emit the event
         emit HashKeySubmitted(hashKey);
+    }
+
+    // Function to check if a hash key is stored
+    function isHashStored(bytes32 hashKey) public view returns (bool) {
+        return storedHashes[hashKey];
     }
 }
